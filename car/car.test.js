fc19b02e41ed7b6eacc27599c89b492165d8bc20
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-16 18:50:33 +0100

    wip

--- car/car.test.js
+++ car/car.test.js
@@ -14,20 +14,18 @@ describe('Car class', () => {
   let prius
   beforeEach(() => {
     // anything you want happening before each test
+    prius = new Car('toyota', 'prius')
   })
   it('is defined', () => {
     expect(Car).toBeDefined()
   })
   it('can be used to make instances of Cars', () => {
-    const prius = new Car('toyota', 'prius') // set up or action
     expect(prius).toBeInstanceOf(Car)
   })
   it('cars have a make property', () => {
-    const prius = new Car('toyota', 'prius')
     expect(prius.make).toBe('toyota')
   })
   it("cars have a model property", () => {
-    const prius = new Car("toyota", "prius")
     expect(prius.model).toBe("prius");
   })
 })
