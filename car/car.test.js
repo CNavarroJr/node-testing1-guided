8d6bc35a1cd846dea381e9d9d40337417fc68bf8
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-16 18:23:55 +0100

    wip

--- car/car.test.js
+++ car/car.test.js
@@ -3,7 +3,7 @@ const Car = require('./car')
 describe('Car class', () => {
   it('works', () => { // THIS is the test
     // assertions
-    expect(2 + 2).toBe(4)
+    expect(2 + 2).toBe(5)
   })
   test('everything working', () => { // THIS is another test
 
